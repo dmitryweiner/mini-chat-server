45d4187c8cb05db714bcda344cd79cc71ee7756c
Dmitry Weiner <dmitry.weiner@gmail.com>
2020-10-26 12:46:08 +0700

    Changed port

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const { initDb } = require('./db');
 initDb();
 const server = require('./server');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 server.listen(PORT, '0.0.0.0', () => console.log(`Server is live at localhost:${PORT}`));
