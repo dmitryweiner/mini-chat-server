ea565add97bd5958ad0e1e9665f1d93675c19026
dmitryweiner <dmitry.weiner@gmail.com>
2020-12-03 12:59:14 +0700

    Messing with sameSite cookie attribute

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -15,10 +15,8 @@ router.post('/', (req, res) => {
   res.cookie(TOKEN_COOKIE_NAME, token, {
     maxAge: 24 * 60 * 60 * 1000, // TODO: to const
     httpOnly: true,
-    sameSite: 'lax'
-    // should uncomment when HTTPS is set
-    // sameSite: 'none'
-    // secure: process.env.NODE_ENV === 'production'
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    secure: process.env.NODE_ENV === 'production'
   });
   res.json({ token });
 });
